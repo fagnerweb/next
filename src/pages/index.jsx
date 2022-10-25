7253d52684df934232ed5b97d669826351ce8144
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-25 12:24:40 -0300

    Initial

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  background-color: ${({theme}) => theme.colors.secondaryColor};
+`;
+
+export default function Home() {
+  return (
+    <Heading>Oi</Heading>
+  )
+}
