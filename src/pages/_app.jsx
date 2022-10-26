128e9fa3684e5624d67fba17717a80274d79909d
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-26 10:10:22 -0300

    Prettier, eslint, storybook and editorConfig

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from "styled-components";
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />    
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.propTypes = AppProps;
